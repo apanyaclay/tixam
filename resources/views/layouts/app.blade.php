f6ab9ec669b9a946f3c20f26b50d6bb8bfd930dd
apanyaclay <apanyaclay@gmail.com>
2020-11-25 01:09:07 +0700

    'add'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -313,7 +313,7 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.0
+      <b>Version</b> 1.0
     </div>
     <strong>Copyright &copy; 2020-{{ date('Y') }} <a href="https://apalay.com" target="_blank">Apalay</a>.</strong> All rights
     reserved.
